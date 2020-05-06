7d366000c073daf4cc421ae6e4752003d76294ff
Guilherme <gui.martinsa@gmail.com>
2020-05-06 13:44:18 -0300

    Implementação do postPostagem

--- blogPessoal/src/app/feed/feed.component.ts
+++ blogPessoal/src/app/feed/feed.component.ts
@@ -9,6 +9,7 @@ import { Postagem } from '../model/Postagem';
 })
 export class FeedComponent implements OnInit {
   listaPostagens: Postagem[]
+  postagem: Postagem = new Postagem
   constructor(private postagemService: PostagemService) { }
 
   ngOnInit() {
@@ -17,6 +18,12 @@ export class FeedComponent implements OnInit {
   findallPostagens(){
     this.postagemService.getAllPostagens().subscribe((resp: Postagem[])=>{
       this.listaPostagens = resp
+    })
+  }
+  publicar(){
+    this.postagemService.postPostagem(this.postagem).subscribe((resp: Postagem)=>{
+      this.postagem = resp
+      location.assign('/feed')
     })
   }
 }
